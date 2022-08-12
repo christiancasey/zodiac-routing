c0c98fa62ebf159cc7bb894e42b23b0c8828afda
christiancasey <christiancasey86@gmail.com>
2022-08-12 12:45:00 +0200

    Updates

---
+++ precache-manifest.9efe64fedf3f7f67363c8ca4906f08a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceb5caad1c9e9fef9a5a",
+    "url": "/zodiac-router/static/css/main.8ac39d64.chunk.css"
+  },
+  {
+    "revision": "ceb5caad1c9e9fef9a5a",
+    "url": "/zodiac-router/static/js/main.ceb5caad.chunk.js"
+  },
+  {
+    "revision": "94f98db40550e255c277",
+    "url": "/zodiac-router/static/js/1.94f98db4.chunk.js"
+  },
+  {
+    "revision": "1768693e37b4c63e15a4",
+    "url": "/zodiac-router/static/js/runtime~main.1768693e.js"
+  },
+  {
+    "revision": "1df36da5cbe9565baa2eea0a160adf6b",
+    "url": "/zodiac-router/static/media/dual.1df36da5.woff"
+  },
+  {
+    "revision": "56c6ee3d3b7a01dcc873cb31b216db3b",
+    "url": "/zodiac-router/static/media/zodiac_constellations.56c6ee3d.svg"
+  },
+  {
+    "revision": "c7f8b09b27cb90fc1b660d7efb185c33",
+    "url": "/zodiac-router/static/media/book-antiqua.c7f8b09b.woff"
+  },
+  {
+    "revision": "50586724041fdb29fe534d359dce941b",
+    "url": "/zodiac-router/index.html"
+  }
+];
